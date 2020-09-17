ab89140bb0826b1ff61b9942a185ebe4a1bc4ca0
Jody Dahmer <jodydahmer@gmail.com>
2020-09-17 18:30:51 -0400

    Basic Python Ideas Added using '#'

---
+++ map.py
@@ -0,0 +1,12 @@
+#This is the first thing I am doing using Python! Hello World!
+#
+
+
+
+# Ideas
+# 
+#Scooters - Lou Metro Open Data
+#Map Data Google Maps KSV Export
+
+
+#How to use MyMaps with Python
