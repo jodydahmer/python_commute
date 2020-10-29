bda3937f5dfcb7c973cbc8635df94466417e5ddf
Jody Dahmer <jodydahmer@gmail.com>
2020-10-29 18:34:05 -0400

    Added class practice code and matplotlibs practice for mapping commutes

--- Request.py
+++ Request.py
@@ -1,14 +1,35 @@
-request = input("What would you like?  ")
-while "please" not in request:
-    request = input("You seem to be missing a magic word.  What would you like?  ")
-print("Here you go!")
-
-
-
 Hours_Day = 1
 
 Hours_remaining = 24 
 
 #Output how many hours are remaining in a day for a commute scheduling app
 
-print("There are {} hours remaining in your day".format(Hours_remaining)
+print("There are {} hours remaining in your day".format(Hours_remaining))
+
+#Gather user commute time and assign it to a new variable
+
+commute_time = input("What is your personal daily commute time?")
+
+#Prompt user by name and ask how many hours they drive
+user_commute_time = input("How long is your daily round-trip commute, {}?" .format(commute_time))
+user_commute_time = int(user_commute_time)
+#Calculate total time spent in day commuting by converting commute time into hours
+daily_commute = user_commute_time / 60
+print("Your daily commute time is {} days".format(daily_commute))
+#Output the price to the screen 
+
+
+#Ask user if they want to proceed
+
+should_proceed = input("Do you want to proceed? y/n")
+
+
+if should_proceed.lower() == "y" : 
+
+    #Print out to the screen difference in hours when days total is displayed
+        print("You have {} hours remaining in your day once we subtract your commute!".format(Hours_remaining))
+    #And then decrement the commute time total from  total hours remaining in day 
+
+Hours_remaining -= daily_commute
+
+
---
+++ matplotlib_practice.py
@@ -0,0 +1,55 @@
+#Class definition practice, should revise features to average commute times of cities
+
+class Person:
+
+    def __init__(self, name, surname, birthdate, address, telephone, email):
+        self.name = name
+        self.surname = surname
+        self.birthdate = birthdate
+
+        self.address = address
+        self.telephone = telephone
+        self.email = email
+
+    def age(self):
+        today = datetime.date.today()
+        age = today.year - self.birthdate.year
+
+        if today < datetime.date(today.year, self.birthdate.month, self.birthdate.day):
+            age -= 1
+
+        return age
+
+person = Person(
+    "Jane",
+    "Doe",
+    datetime.date(1992, 3, 12), # year, month, day
+    "No. 12 Short Street, Greenville",
+    "555 456 0987",
+    "jane.doe@example.com"
+)
+
+print(person.name)
+print(person.email)
+print(person.age())
+
+
+
+
+#I need to make sure I can use the mapping feature to use my geodata
+
+
+
+
+
+
+from matplotlib import pyplot
+
+fig, ax = pyplot.subplots()
+ax.plot([commute.day for commute in commutes],
+        [commute.took.total_seconds() / 60 for commute in commutes])
+
+ax.set(xlabel='day', ylabel='commute (minutes)',
+       title='Daily commute')
+ax.grid()
+pyplot.show()
