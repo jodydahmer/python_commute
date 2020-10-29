6079c367f37dab785b845a6db608c8981af6da4f
Jody Dahmer <jodydahmer@gmail.com>
2020-10-29 10:58:45 -0400

    Experimenting with basic commute timer out of 24 hour day period. User inputs commute time, python will execute calculations out of 24 hours cycle and use for-while loops to output strings.

--- Request.py
+++ Request.py
@@ -2,3 +2,13 @@ request = input("What would you like?  ")
 while "please" not in request:
     request = input("You seem to be missing a magic word.  What would you like?  ")
 print("Here you go!")
+
+
+
+Hours_Day = 1
+
+Hours_remaining = 24 
+
+#Output how many hours are remaining in a day for a commute scheduling app
+
+print("There are {} hours remaining in your day".format(Hours_remaining)
